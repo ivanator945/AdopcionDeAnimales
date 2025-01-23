9eea6d80027db21ba8e60f07fc3d17b06afeea5b
ivan <ivansanchezdominguez1@gmail.com>
2025-01-23 23:31:33 +0100

    añadiendo comentarios necesarios para clasificar los bloques etc...

--- js/Animaciones.js
+++ js/Animaciones.js
@@ -1,8 +1,3 @@
-// Mostrar la fecha actual
-document.getElementById("current-date").textContent = new Date().toLocaleDateString("es-ES", {
-    weekday: "long", year: "numeric", month: "long", day: "numeric"
-});
-
 // Para validar el formulario
 const form = document.getElementById('surveyForm');
 const submitButton = document.getElementById('submitButton');
